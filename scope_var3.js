570126cf453034612793cfe608cb873e52290ae9
Nilesh410 <shirudenileshs@gmail.com>
2023-12-05 00:01:44 +0530

    access var variable before first time declare

---
+++ scope_var3.js
@@ -0,0 +1,13 @@
+console.log("num from the outside the function but before the declerationa and initialize:",num)
+var num=10 //global scope
+
+function f() //global scope
+{
+    console.log("num from inside the function but before redeclare and initialize:",num)
+    //output: undefined due to hoisting of var variable 
+    var num=20 //functional scope
+    console.log("num from inside the function:",num)//access the functional scope value
+}
+ 
+f()
+console.log("num from the outside the function:",num)//access the global scope value
