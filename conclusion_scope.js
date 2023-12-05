78b140b2e04823458098afe09ae42175a089323f
Nilesh410 <shirudenileshs@gmail.com>
2023-12-05 11:56:30 +0530

    conclusion of scope and question on that

---
+++ conclusion_scope.js
@@ -0,0 +1,37 @@
+/*
+1) var 
+   -> var is global scope and functional scope
+   -> we can access var variable before declerationa and initalization duw to hoisting
+   -> we can reinitalize var variable in same scope
+   -> we cna redeclare the var variable with same name in same scope
+
+2) let 
+   ->let has block scope 
+   -> block scope varible can not access before decleration due to TDZ
+   -> it can reinitalize in the same scope 
+   -> but we can not redeclare in the same scope with same name
+
+3) const:-
+   ->same as let variable 
+   -> but only difference is we can reinitalize or change the value of const variable
+
+Questions Related with the scoping:-
+1) what is scope of let, var and const variable
+2) can we redeclare and re initialize the let,var and const type of variable
+3) How JS program work internally?
+4) what is lexical scoping?
+5) what is hoisting?
+6) what is temporal dead zone?
+7) what is auto global of variable?
+8) How to avoid auto global process?
+9) what is stack ?
+10) what is call stack?
+11) what is execution context?
+12) what is global execution context?
+
+
+
+
+
+
+*/
