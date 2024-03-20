7228ce9390e07c2af81b0f057fbc602ad91fd9d6
Nilesh410 <shirudenileshs@gmail.com>
2024-03-20 17:21:35 +0530

    Try function invocation with and without return

--- function/example1.js
+++ function/example1.js
@@ -8,35 +8,40 @@
  */
 
 //demo1
-function calculateaddition(num1,num2)
-{
-   console.log("addition=",num1+num2);
-}
-calculateaddition(2,3)  //output:- 5
-calculateaddition(2,"abc") //output:- 2abc
-calculateaddition(2,'n') //output:- 2n
-calculateaddition(2,12.5) //output:- 14.5
-calculateaddition(2,false) //output:- 2
-calculateaddition(2,undefined) //output:- Nan
-calculateaddition(2,null) //output:- 2
-
-//demo2
-console.log(typeof(undefined)) //type of undefined -> undefined
-console.log(typeof(NaN)) //type of Nan -> number
-console.log(typeof(null)) //type of null-> object
-console.log(typeof(calculateaddition)) //type of function -> function 
-
-
-
-
 // function calculateaddition(num1,num2)
 // {
 //    console.log("addition=",num1+num2);
 // }
+// calculateaddition(2,3)  //output:- 5 number
+// calculateaddition(2,"abc") //output:- 2abc string 
+// calculateaddition(2,'n') //output:- 2n string
+// calculateaddition(2,12.5) //output:- 14.5 number
+// calculateaddition(2,false) //output:- 2 number
+// calculateaddition(2,undefined) //output:- Nan number
+// calculateaddition(2,null) //output:- 2 number
 
-// calculateaddition(2,3)
-//output: addition=5
+// //demo2
+// console.log(typeof(undefined)) //type of undefined -> undefined
+// console.log(typeof(NaN)) //type of Nan -> number
+// console.log(typeof(null)) //type of null-> object
+// console.log(typeof(calculateaddition)) //type of function -> function 
 
+
+
+//Demo 3
+function calculateaddition1(num1,num2)
+{
+   let result=num1+num2;
+   console.log(result)
+   //return 
+   //return result
+}
+
+// let result=calculateaddition1(2,3) 
+// console.log(result)
+//calculateaddition1(2,3)
+console.log(calculateaddition1(2,3))
+n
 /* 
 //Questions :-
 1)what are the different data types in JS
