d723b19ef5f49805ec8754272bdca41667df9d06
Nilesh410 <shirudenileshs@gmail.com>
2024-03-21 17:20:44 +0530

    add function hoisting concept

---
+++ function_topic/example2.js
@@ -0,0 +1,31 @@
+/* Que 1) what is function hoising ? 
+Ans:- calling the function before its declaration and definition and its execute the definition
+Function hoising is applicable only to function decleration not the function expression properties
+*/
+
+//1
+/* sayMyname("Nilesh") //output :- function execution result
+function sayMyname(name)
+{
+    console.log(`My name is ${name}`)
+} */
+
+
+//2
+// myname("Nilesh") //output: error-> can not access myname before initialization
+// let myname=function(name)
+// {
+//    console.log(`my name is ${name}`) 
+// }
+
+//3
+// myname("Nilesh") //output error same as above like anonymous function 
+// let myname=(name)=>{
+//     console.log(`my name is ${name}`)
+// }
+
+myname("NSS")  //same error
+let myname=new Function('name','console.log(`my name is ${name}`)')
+
+
+
