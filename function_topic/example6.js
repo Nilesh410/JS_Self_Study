c7e18b1dafd422cadba078b54fc097a108657ae3
Nilesh410 <shirudenileshs@gmail.com>
2024-03-22 09:55:14 +0530

    Update the all files

--- function_topic/example6.js
+++ function_topic/example6.js
@@ -1,1 +1,14 @@
-//How the function is return the 
+//How the function has return a other function
+//Higher Order Function 
+function reutrnfunc(message){
+    console.log(message)
+    return function(){
+        return message+"Developer"
+    }
+}
+reutrnfunc("NSS") //NSS 
+reutrnfunc("NSS")()//NSS 
+console.log(reutrnfunc("NSS")())//NSS NSSDeveloper
+let result=reutrnfunc("NSS")
+console.log(result)//NSS function 
+console.log(result()) //NSSDeveloper
