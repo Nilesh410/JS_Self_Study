3f854399e21c171e33c1bdf0ebb67e5324f81f5c
Nilesh410 <shirudenileshs@gmail.com>
2024-03-27 11:33:44 +0530

    add use of increment and decrement operator

---
+++ operators_use/increment_decrement.js
@@ -0,0 +1,18 @@
+//Use of Increment and Decrement Operator 
+
+/* let x=3
+let y=x++
+console.log(`x:${x},y:${y}`) //x=4,y=3
+*/
+
+/* const x=3
+const y=x++
+console.log(`x:${x},y:${y}`)//error-const cant be change the value */
+
+/* let x=3
+let y=++x
+console.log(`x:${x},y:${y}`) //x=4,y=4 */
+
+const x=3
+const y=++x
+console.log(`x:${x},y:${y}`)//cant peroform the operation on const variable
