24d7ccbea1e31775897ec2e50a539243cd56ca3f
Nilesh410 <shirudenileshs@gmail.com>
2024-03-28 18:28:35 +0530

    add examples using comparison operator

---
+++ operators_use/comparison_operator.js
@@ -0,0 +1,20 @@
+/* console.log(2>1)
+console.log(2>=1)
+console.log(2<1)
+console.log(2==1)
+console.log(2!=1) */
+
+/* console.log("2">1) //true => string 2 convert into number
+console.log("02">1)//true */
+
+/* console.log(null>0) //false
+console.log(null<0) //false
+console.log(null==0) //false
+console.log(null>=0) //false */
+
+//< ,>,>= ,<= and  == works differntly 
+
+//=== strict check
+console.log("2"===2)//false=> check value with data types 
+console.log("2"==2) //true
+
