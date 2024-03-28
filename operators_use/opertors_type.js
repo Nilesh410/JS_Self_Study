13a96f155cb81c80b4662b0eed0d4dc5286a05a8
Nilesh410 <shirudenileshs@gmail.com>
2024-03-28 20:25:13 +0530

    add all different types of operator

---
+++ operators_use/opertors_type.js
@@ -0,0 +1,30 @@
+//Type of operator 
+/* 
+-----Unary Operator------------
+1. delete operator => a delete operator occurs within strict mode code,
+2. void operator
+3. typeof operator
+4. unary + operator => a delete operator occurs within strict mode code, + UnaryExpression
+5. unary - operator => unary - operator converts its operand to a numeric value and then negates it.- UnaryExpression
+6. Bitwise ~ not operator 
+   UnaryExpression : ~ UnaryExpression
+7. Logical ! not operator => UnaryExpression : ! UnaryExpression
+
+------Expoential Operator-------- 
+8. exponential oeprator **
+
+-----Multiplicative Operator------
+9. Multiplicative Operator *,/,%
+10. Additive and Substraction Operator +, - 
+11. Bitwise Shift Operator 
+   A.left shift operator << -> Performs a bitwise left shift operation on the left operand by the amount specified by the right operand.
+   B.SignedRight Shift Operator >> -> Performs a sign-filling bitwise right shift operation on the left operand by the amount specified by the right operand.
+   C. Unsigned Right Shift Operator >>> -> Performs a zero-filling bitwise right shift operation on the left operand by the amount specified by the right operand.
+12. Relational Operator => <, >, >=, <= relational operator 
+13. equality operator => ==, === is strictly equal 
+14. Binary Bitwise Operator => & ^ |
+15. Binary Logical Operator => && ^^ ||
+16. Conditional Operator => ?:
+17. Assignment Operator +=
+18. Comma Operatot => ,
+*/
