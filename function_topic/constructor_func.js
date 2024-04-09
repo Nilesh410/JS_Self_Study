21cbf17ae396cbe99b89f13d9e844befe833df8f
Nilesh410 <shirudenileshs@gmail.com>
2024-04-09 23:57:23 +0530

    add constructor functional example

---
+++ function_topic/constructor_func.js
@@ -0,0 +1,2 @@
+const func=new Function('value','return value*value');
+console.log(func(2));
--- function_topic/example1.js
+++ function_topic/example1.js
@@ -99,6 +99,10 @@ myname("Nilesh") */
 })("Nilesh Shirude") */
 
 //5 Constructor Function 
-
+/* The Function() constructor creates Function objects. 
+Calling the constructor directly can create functions dynamically, 
+but suffers from security and similar (but far less significant) performance issues as eval().
+ However, unlike eval (which may have access to the local scope), 
+the Function constructor creates functions which execute in the global scope only. */
 let myname=new Function('name','console.log(`my name is ${name}`)')
 myname("NSS")
