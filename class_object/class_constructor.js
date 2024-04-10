ef7233f699bdbae7ba401795b7cf64a43207f7c7
Nilesh410 <shirudenileshs@gmail.com>
2024-04-10 20:14:59 +0530

    add class constructor example

---
+++ class_object/class_constructor.js
@@ -0,0 +1,12 @@
+class Student{
+    constructor(name,lastname,age,gender)
+    {
+        this.s_name=name;
+        this.s_lastname=lastname;
+        this.s_age=age;
+        this.s_gender=gender;
+    }
+}
+
+const stud1=new Student('Nilesh','Shirude',34,'Male')
+console.log(stud1);
