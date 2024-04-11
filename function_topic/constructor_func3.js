ffc6b50eb3df046aca77f02d9a21a0c216a8ed54
Nilesh410 <shirudenileshs@gmail.com>
2024-04-11 16:12:45 +0530

    add constructor function example

---
+++ function_topic/constructor_func3.js
@@ -0,0 +1,13 @@
+var Node=function(value)
+{
+    this.data=value;
+    this.next=null;
+}
+
+var Linkedlist=function()
+{
+    this.head=null
+}
+
+console.log(new Node(10));
+console.log(typeof(new Node(10)));
