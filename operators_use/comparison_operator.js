1ee8e4d559063f741d4baf43369d604a7403c99b
Nilesh410 <shirudenileshs@gmail.com>
2024-04-25 23:44:32 +0530

    add file

--- operators_use/comparison_operator.js
+++ operators_use/comparison_operator.js
@@ -1,20 +1,20 @@
-/* console.log(2>1)
-console.log(2>=1)
-console.log(2<1)
-console.log(2==1)
-console.log(2!=1) */
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
 
 /* console.log("2">1) //true => string 2 convert into number
 console.log("02">1)//true */
 
-/* console.log(null>0) //false
+console.log(null>0) //false
 console.log(null<0) //false
 console.log(null==0) //false
-console.log(null>=0) //false */
+console.log(null>=0) //true
 
 //< ,>,>= ,<= and  == works differntly 
 
 //=== strict check
-console.log("2"===2)//false=> check value with data types 
-console.log("2"==2) //true
+/* console.log("2"===2)//false=> check value with data types 
+console.log("2"==2) //true */
 
