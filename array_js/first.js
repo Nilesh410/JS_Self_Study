9b99e9e5407bb8da17f17b7588ce9d16d1997a84
Nilesh410 <shirudenileshs@gmail.com>
2024-05-21 20:50:59 +0530

    array decleration in JAVA Script

---
+++ array_js/first.js
@@ -0,0 +1,9 @@
+/* Array :- Collection of elements of different data types 
+Syntax :- let variable_name=[elements value]
+       or let variable_name=new Array(array element with comma seperated) */
+
+let arr1=[10,20,"ABC",true,23.2]
+console.log("arr1=",arr1);
+
+let arr2=new Array(25,12,"PQR",false);
+console.log("arr2=",arr2);
