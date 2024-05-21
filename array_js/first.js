44dce2d1da3b82e864f4a6be5d3e58b302959751
Nilesh410 <shirudenileshs@gmail.com>
2024-05-21 23:40:24 +0530

    use length property on array

--- array_js/first.js
+++ array_js/first.js
@@ -2,10 +2,10 @@
 Syntax :- let variable_name=[elements value]
        or let variable_name=new Array(array element with comma seperated) */
 
-let arr1=[10,20,"ABC",true,23.2]
+const arr1=[10,20,"ABC",true,23.2]
 console.log("arr1=",arr1);
 
-let arr2=new Array(25,12,"PQR",false);
+const arr2=new Array(25,12,"PQR",false);
 console.log("arr2=",arr2);
 
 arr1.push("LMN");
@@ -19,3 +19,6 @@ arr2.unshift(undefined);
 console.log("arr2=",arr2);
 arr2.shift();
 console.log("arr2=",arr2);
+
+console.log(arr1.length);
+console.log(arr2.length);
