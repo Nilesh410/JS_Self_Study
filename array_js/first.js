8e2e77d01096394ec3fd96ef698207c5df780b2c
Nilesh410 <shirudenileshs@gmail.com>
2024-05-21 20:56:15 +0530

    study use of push pop unshift and shift

--- array_js/first.js
+++ array_js/first.js
@@ -7,3 +7,15 @@ console.log("arr1=",arr1);
 
 let arr2=new Array(25,12,"PQR",false);
 console.log("arr2=",arr2);
+
+arr1.push("LMN");
+arr1.push(null);
+console.log("arr1=",arr1);
+arr1.pop();
+console.log("arr1=",arr1);
+
+arr2.unshift("DEF");
+arr2.unshift(undefined);
+console.log("arr2=",arr2);
+arr2.shift();
+console.log("arr2=",arr2);
