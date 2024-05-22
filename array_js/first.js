1c7c4f6b4a9a3ab1fe618db6b6638e575acbba11
Nilesh410 <shirudenileshs@gmail.com>
2024-05-22 15:13:59 +0530

    add name of method

--- array_js/first.js
+++ array_js/first.js
@@ -1,6 +1,15 @@
 /* Array :- Collection of elements of different data types 
 Syntax :- let variable_name=[elements value]
-       or let variable_name=new Array(array element with comma seperated) */
+       or let variable_name=new Array(array element with comma seperated) 
+
+Array method:-
+1.length
+2.push 
+3.pop
+4.unshift
+5.shift
+6.
+*/
 
 const arr1=[10,20,"ABC",true,23.2]
 console.log("arr1=",arr1);
