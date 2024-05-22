08f395ecad05cb9c9249e57d0f9e72d14fec0bb6
Nilesh410 <shirudenileshs@gmail.com>
2024-05-22 15:08:22 +0530

    array with different type of decl

--- array_js/second.js
+++ array_js/second.js
@@ -1,11 +1,11 @@
 //check the declaration of array varible with var, let and const and observed the different properties
 //var :- redeclare, reassign and manipulate the array
 //let :- reassign and manipulate the array elements 
-//const :- only manipulation is possible
+//const :- only manipulation is possible, reassign its not possible
 
-var arr1=new Array("Nilesh","Jyoti","Ridant");
+const arr1=new Array("Nilesh","Jyoti","Ridant");
 console.log("arr1=",arr1);
 arr1.push("Shirude");
 console.log("arr1=",arr1);
-var arr1=["Shirude","Ajay","Soni"];
+arr1=["Shirude","Ajay","Soni"];
 console.log("arr1=",arr1);
