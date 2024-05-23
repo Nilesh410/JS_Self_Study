53b9da73378b4ff47fbee0470ffb85096fee9d7d
Nilesh410 <shirudenileshs@gmail.com>
2024-05-23 13:11:42 +0530

    study basic things of object and access each element

--- object_study/object_basic.js
+++ object_study/object_basic.js
@@ -6,11 +6,24 @@
     Object.create();
     
 */
+const address=Symbol("add")
 const obj={
     name:"NSS",
     id:559,
     emailid:"shirudenileshs@gmail.com",
     mobileno_active:true,
+    "address":"Flat No 505,Atharva, Mohan Nagar Cooperative Housign Society",
+    fullname:{
+        firstname:"Nilesh",
+        lastname:"Shirude"
+    },
+    info:function(){
+       console.log(`candidate first name=${this.fullname.firstname} and last name=${this.fullname.lastname}`)
+    }
 }
-
 console.log(obj);
+// console.log(obj.emailid);
+// console.log(obj["name"]);
+// console.log(obj["address"]);
+console.log(obj.fullname);
+console.log(obj.info())
