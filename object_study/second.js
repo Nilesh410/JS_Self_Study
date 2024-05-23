c58cdd4165234c0db97c47df73b0b0ee2acfdfdf
Nilesh410 <shirudenileshs@gmail.com>
2024-05-23 19:49:20 +0530

    merge two object

---
+++ object_study/second.js
@@ -0,0 +1,22 @@
+const obj1={
+    name:"NSS",
+    id:559,
+    emailid:"shirudenileshs@gmail.com",
+    mobileno_active:true,
+    address:"Flat No 505,Atharva, Mohan Nagar Cooperative Housign Society",
+    fullname:{
+        firstname:"Nilesh",
+        lastname:"Shirude"
+    },
+    info:function(){
+       console.log(`candidate first name=${this.fullname.firstname} and last name=${this.fullname.lastname}`)
+    }
+}
+const obj2={name:"Nilesh Shirude"}
+
+//1st way to add two objects
+//const obj3={obj1,obj2}
+//const obj3=Object.assign(obj1,obj2);//previous name key value replace by new one 
+const obj3={...obj1,...obj2}
+console.log(obj3)
+
