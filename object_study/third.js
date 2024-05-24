b51e2595d42b2c554d5d6171321eb9be3e6b14b8
Nilesh410 <shirudenileshs@gmail.com>
2024-05-24 14:58:56 +0530

    add array of object

---
+++ object_study/third.js
@@ -0,0 +1,21 @@
+const arr=[
+    {
+        std_id:22101,
+        name:"ABC"
+    },
+    {
+        std_id:22102,
+        name:"DEF"
+    },
+    {
+        std_id:22103,
+        name:"LMN"
+    },
+    {
+        std_id:22104,
+        name:"PQR"
+    }
+]
+console.log(arr)//show complete array
+console.log(arr[0])//access indevidual element of array
+console.log(arr[1].name)//access indevidual property of object from the array
