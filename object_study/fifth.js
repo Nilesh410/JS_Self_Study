6bc01964164a71531f4a221a899d40fcb226ecb5
Nilesh410 <shirudenileshs@gmail.com>
2024-05-24 15:56:44 +0530

    add json data

---
+++ object_study/fifth.js
@@ -0,0 +1,66 @@
+{
+    "results": [
+      {
+        "gender": "male",
+        "name": {
+          "title": "Mr",
+          "first": "Wilhelmus",
+          "last": "Van Hasselt"
+        },
+        "location": {
+          "street": {
+            "number": 5722,
+            "name": "Gerrit Vermootenstraat"
+          },
+          "city": "Nuis",
+          "state": "Drenthe",
+          "country": "Netherlands",
+          "postcode": "5446 FQ",
+          "coordinates": {
+            "latitude": "35.1168",
+            "longitude": "-100.4073"
+          },
+          "timezone": {
+            "offset": "-3:00",
+            "description": "Brazil, Buenos Aires, Georgetown"
+          }
+        },
+        "email": "wilhelmus.vanhasselt@example.com",
+        "login": {
+          "uuid": "fd599907-b348-4a3b-a768-4896bede282e",
+          "username": "happylion376",
+          "password": "javier",
+          "salt": "SmrHPmS6",
+          "md5": "69d97757b092663065676c23291c9094",
+          "sha1": "15c4be9a89ac6568a3493d1c81a8669c4869a959",
+          "sha256": "5fd499acc050258f02139d257f4f24967d3fa148fc44cd48dd2b51915bb8900e"
+        },
+        "dob": {
+          "date": "1974-07-08T08:26:06.187Z",
+          "age": 49
+        },
+        "registered": {
+          "date": "2014-08-08T17:40:18.113Z",
+          "age": 9
+        },
+        "phone": "(079) 8915192",
+        "cell": "(06) 97107233",
+        "id": {
+          "name": "BSN",
+          "value": "20406773"
+        },
+        "picture": {
+          "large": "https://randomuser.me/api/portraits/men/42.jpg",
+          "medium": "https://randomuser.me/api/portraits/med/men/42.jpg",
+          "thumbnail": "https://randomuser.me/api/portraits/thumb/men/42.jpg"
+        },
+        "nat": "NL"
+      }
+    ],
+    "info": {
+      "seed": "f7f248470b2fc011",
+      "results": 1,
+      "page": 1,
+      "version": "1.4"
+    }
+  }
