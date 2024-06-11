028ca85d7467cd95cd84c2da3c688b87877986c6
Nilesh410 <shirudenileshs@gmail.com>
2024-06-11 12:03:12 +0530

    add practice example on compartor operator

---
+++ practice_folder/first.js
@@ -0,0 +1,11 @@
+/* console.log(1<2<3); //true 
+console.log(1<2) //true 
+console.log(true<3) //true  */
+
+/* console.log(1>2>3)//false
+console.log(1>2)//false
+console.log(false>3)//false */
+
+console.log(1>2<3)//true
+console.log(1>2)//false
+console.log(false<3)//true
