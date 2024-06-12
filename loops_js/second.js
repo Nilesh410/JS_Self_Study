9cda3ef25fca9bd918c979d2f4a6deb81c9d48d2
Nilesh410 <shirudenileshs@gmail.com>
2024-06-12 09:30:28 +0530

    add while example

--- loops_js/second.js
+++ loops_js/second.js
@@ -2,10 +2,10 @@ const arr=["Jaipur","Mumbai","Luknow","Baroda","Raipur"]
 let index=0;
 while(index<arr.length)
 {
-    console.log(`arr element at index ${index}= ${arr[index]}`)
     if(arr[index]==="Luknow")
     {  index++
        continue
     }
+    console.log(`arr element at index ${index}= ${arr[index]}`)
     index++
 }
