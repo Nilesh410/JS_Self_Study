c49f0bf61dd361c162a179f7c503f3245a9d1069
Nilesh410 <shirudenileshs@gmail.com>
2024-06-13 11:49:11 +0530

    add updated code for occurence of code

--- practice_folder/example2.js
+++ practice_folder/example2.js
@@ -1,18 +1,25 @@
 //Find out the how many vowels and their occurence from the given string 
-const str="Nilesh Suresh Shiurde"
+//const str="Nilesh Suresh Shiurde"
+let str="Aabb Eedd Iikk Oomm Uujj"
 let count_vowel=0
 let vowel_occur={}
-for (const ele of str) {
+for (let ele of str) {
     if(ele==='a'||ele==='e'||ele==='i'||ele==='o'||ele==='u'||
        ele==='A'||ele==='E'||ele==='I'||ele==='O'||ele==='U')
        {
           count_vowel+=1;
-          if(vowel_occur[ele])
+          if(ele.localeCompare(ele.toUpperCase())===0)
           {
-            vowel_occur[ele]+=1
+             ele=ele.toLowerCase()
           }
-          else
-            vowel_occur[ele]=1
+                if(vowel_occur[ele])
+                {
+                  vowel_occur[ele]+=1
+                }
+                else
+                vowel_occur[ele]=1
+          
+          
        }
 }
 console.log(`No of vowels from the given string is ${count_vowel} and occurence of each vowel is`,vowel_occur)
