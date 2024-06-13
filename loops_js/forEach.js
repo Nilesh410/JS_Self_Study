6f104027f75a9bc83f0205f566e507f7dbb23c74
Nilesh410 <shirudenileshs@gmail.com>
2024-06-13 12:20:40 +0530

    use of process.stdout.write(ele)

---
+++ loops_js/forEach.js
@@ -0,0 +1,19 @@
+let arr=[1,2,3,4,5,6,7,8,9,10]
+//1
+arr.forEach((ele)=>{
+    console.log(ele) //it shows the result vertically on terminal, we want horixonatlly then how
+})
+
+//2
+let output=""
+arr.forEach((ele)=>
+{
+    output+=ele+" "
+})
+console.log(output) //show the same result horizontally
+console.log(output.trim())
+
+//3
+arr.forEach((ele)=>{
+    process.stdout.write(ele+" ") //this works for only node js environment not for browser environment
+})
