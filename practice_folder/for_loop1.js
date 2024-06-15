c5d94988c3489945fbf68cda640f8195fe068d75
Nilesh410 <shirudenileshs@gmail.com>
2024-06-15 23:58:01 +0530

    add for example with diff decler

--- practice_folder/for_loop1.js
+++ practice_folder/for_loop1.js
@@ -5,9 +5,20 @@ for(let i=0;i<5;i++)
     },5)
 }
 
+/* for(var i=0;i<5;i++)
+{
+        setTimeout(()=>{
+            console.log(i)
+        },5)
+   
+} */
 for(var i=0;i<5;i++)
 {
-    setTimeout(()=>{
-        console.log(i)
-    },5)
+    function number(i)
+    {
+        setTimeout(()=>{
+            console.log(i)
+        },5)
+    }
+   number(i)
 }
