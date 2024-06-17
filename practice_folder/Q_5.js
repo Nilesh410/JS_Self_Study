f2265a9a5546531ea7883abab512b02d255b8d7b
Nilesh410 <shirudenileshs@gmail.com>
2024-06-17 23:45:46 +0530

    update the file

--- practice_folder/Q_5.js
+++ practice_folder/Q_5.js
@@ -3,4 +3,5 @@ console.log(true);
 //console.log(++true);//invalid left side expression in prefix operation 
 console.log(+true)//1=>convert the value into number
 console.log(+2)//2
-console.log(!"Hello") //false
+console.log(!"Hello") //false=> always convert to binary 
+console.log(!2)
