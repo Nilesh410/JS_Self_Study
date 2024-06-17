ff70689e9173d526019ce576ba351bce4d756647
Nilesh410 <shirudenileshs@gmail.com>
2024-06-17 23:44:25 +0530

    use of unary + operator and negation operator

---
+++ practice_folder/Q_5.js
@@ -0,0 +1,6 @@
+console.log(true); 
+//console.log(true++);//invalid left side expression in postfix operation 
+//console.log(++true);//invalid left side expression in prefix operation 
+console.log(+true)//1=>convert the value into number
+console.log(+2)//2
+console.log(!"Hello") //false
