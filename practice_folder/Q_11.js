725526b2967d0ae0be51518ac045bc8622b9264d
Nilesh410 <shirudenileshs@gmail.com>
2024-06-18 00:13:39 +0530

    function object

---
+++ practice_folder/Q_11.js
@@ -0,0 +1,10 @@
+function show(firstname,lastname)
+{
+    this.firstname=firstname
+    this.lastname=lastname
+}
+
+const show1=new show("Mr","X")
+const show2=show("Nilesh","Shirude")
+console.log(show1)
+console.log(show2)
