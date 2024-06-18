c550989d03f388a9f39510da83424b61efa8d400
Nilesh410 <shirudenileshs@gmail.com>
2024-06-18 11:42:45 +0530

    calling a function with template literal

---
+++ practice_folder/Q_12.js
@@ -0,0 +1,11 @@
+function getSummary(one,two,three)
+{
+    console.log(one)
+    console.log(two)
+    console.log(three)
+}
+const fname="Nilesh"
+//const age=34
+
+getSummary `${fname} age is ${age}`//calling function with firt parameter as a array of string without variable value 
+//and remaining prameters are replace with variable values
