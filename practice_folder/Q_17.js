c471bf06de28c78d9fe9938de55ec6d856110d08
Nilesh410 <shirudenileshs@gmail.com>
2024-06-18 11:58:42 +0530

    call function with object as an argument

---
+++ practice_folder/Q_17.js
@@ -0,0 +1,11 @@
+function checkAge(data)
+{
+    if(data==={age:18})
+        console.log("You are adult")
+    else if(data=={age:18})
+        console.log("Still you are adult")
+    else
+        console.log("No age")
+}
+
+checkAge({age:18})// no age 
