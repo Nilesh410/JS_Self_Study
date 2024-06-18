b211656e4cf83929eddd5592862b3d1811a5d1ca
Nilesh410 <shirudenileshs@gmail.com>
2024-06-18 12:27:10 +0530

    use prototype

--- practice_folder/Q_23.js
+++ practice_folder/Q_23.js
@@ -1,4 +1,4 @@
-for(let i=1;i<5;i++)
+for(var i=1;i<5;i++)
 {
     if(i===3)
         continue
---
+++ practice_folder/Q_24.js
@@ -0,0 +1,6 @@
+String.prototype.youAreAmazing=()=>{
+   return "You are amazing my friend"
+}
+
+const str="Nilesh"
+console.log(str.youAreAmazing())
